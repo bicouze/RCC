813fe0f8d3f5344d9fa045585f81d6ccc2dadcea
bicouze <benjamin.guillet@live.fr>
2025-02-04 20:01:58 +0100

    Typo

--- src/components/CompensationTable.tsx
+++ src/components/CompensationTable.tsx
@@ -41,7 +41,7 @@ export function CompensationTable() {
               Route
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              customers
+              Customers
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Type
