c9d84d55ed7d3a264c78a93ddc3ca333c49db9eb
bicouze <benjamin.guillet@live.fr>
2025-02-04 22:08:56 +0100

    Mouseover

--- src/components/CompensationChart.tsx
+++ src/components/CompensationChart.tsx
@@ -36,7 +36,11 @@ export function CompensationChart({ type = 'bar', data }: CompensationChartProps
             <Tooltip 
             formatter={(value: number, name: string, props: any) => {
               if (props.payload && props.payload.name) {
-                return [formatValue(value), props.payload.name]; 
+                return [formatValue(value), props.payload.name];
+              } else if (name === 'given') {
+                return [formatValue(value), 'Given'];
+              } else if (name === 'consumed') {
+                return [formatValue(value), 'Consumed'];
               } else {
                 return [formatValue(value), 'Amount'];
               }
